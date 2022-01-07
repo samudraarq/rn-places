93bb5f09ed7e4d04edb49da886a1005cb4d21de9
Samudra Faris Arqam <samudrafaris@gmail.com>
2022-01-07 10:45:41 +0700

    install redux

--- App.js
+++ App.js
@@ -1,10 +1,21 @@
 import { NavigationContainer } from "@react-navigation/native";
 import PlacesNavigator from "./navigation/PlacesNavigator";
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+
+import placesReducer from "./store/places-reducer";
 
 export default function App() {
+  const rootReducer = combineReducers({
+    places: placesReducer,
+  });
+  const store = createStore(rootReducer, applyMiddleware(thunk));
   return (
-    <NavigationContainer>
-      <PlacesNavigator />
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <PlacesNavigator />
+      </NavigationContainer>
+    </Provider>
   );
 }
