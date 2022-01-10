b753806cdfee4ae108c096d6f57e2482bb0d76ac
Samudra Faris Arqam <samudrafaris@gmail.com>
2022-01-10 09:38:32 +0700

    install map view

--- components/MapPreview.js
+++ components/MapPreview.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, Image, View } from "react-native";
+import { StyleSheet, Image, TouchableOpacity } from "react-native";
 import { GOOGLE_API_TOKEN } from "@env";
 
 const MapPreview = (props) => {
@@ -8,13 +8,16 @@ const MapPreview = (props) => {
     imagePreviewUrl = `https://maps.googleapis.com/maps/api/staticmap?center=${props.location.lat},${props.location.lng}&zoom=14&size=400x200&maptype=roadmap&markers=color:red%7Clabel:A%7C${props.location.lat},${props.location.lng}&key=${GOOGLE_API_TOKEN}`;
   }
   return (
-    <View style={{ ...styles.mapPreview, ...props.style }}>
+    <TouchableOpacity
+      onPress={props.onPress}
+      style={{ ...styles.mapPreview, ...props.style }}
+    >
       {props.location ? (
         <Image style={styles.mapImage} source={{ uri: imagePreviewUrl }} />
       ) : (
         props.children
       )}
-    </View>
+    </TouchableOpacity>
   );
 };
 
--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,14 +1,21 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import MapView from "react-native-maps";
 
 const MapScreen = () => {
-  return (
-    <View>
-      <Text>MapScreen</Text>
-    </View>
-  );
+  const mapRegion = {
+    latitude: 37.78,
+    longitude: -122.43,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  };
+  return <MapView region={mapRegion} style={styles.map} />;
 };
 
 export default MapScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  map: {
+    flex: 1,
+  },
+});
